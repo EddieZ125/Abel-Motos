aefdbc1a4bc1dc04bfca276b65b7c49979843d05
PedroLabrador <pedrolabrador3@outlook.com>
2020-03-30 23:51:53 -0400

    Clientes en progreso

---
+++ app/Cliente.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cliente extends Model
+{
+    protected $table = 'clientes';
+    protected $fillable = ['nombre', 'cedula', 'direccion', 'telefono'];
+    protected $dates = ['created_at', 'deleted_at'];
+}
