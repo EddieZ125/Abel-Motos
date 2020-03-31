ce5193e498030be71345a0e9869a597e9ee3e3a5
PedroLabrador <pedrolabrador3@outlook.com>
2020-03-31 16:07:24 -0400

    Acomodando al estandar de laravel

--- database/migrations/2020_03_16_160544_create_factura_producto_table.php
+++ database/migrations/2020_03_16_160544_create_factura_producto_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductoFacturasTable extends Migration
+class CreateFacturaProductoTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateProductoFacturasTable extends Migration
      */
     public function up()
     {
-        Schema::create('producto_facturas', function (Blueprint $table) {
+        Schema::create('factura_producto', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('factura_id');
             $table->unsignedBigInteger('producto_id');
@@ -22,9 +22,12 @@ class CreateProductoFacturasTable extends Migration
             $table->decimal('precio', 15, 2);
             $table->timestamps();
 
-            $table->foreign('factura_id')->references('id')->on('facturas');
-            $table->foreign('producto_id')->references('id')->on('productos');
-            $table->foreign('historial_divisa_id')->references('id')->on('historial_divisas');
+            $table->foreign('factura_id')->references('id')->on('facturas')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('producto_id')->references('id')->on('productos')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('historial_divisa_id')->references('id')->on('historial_divisas')
+                ->onUpdate('cascade')->onDelete('cascade');
         });
     }
 
@@ -35,6 +38,6 @@ class CreateProductoFacturasTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('producto_facturas');
+        Schema::dropIfExists('factura_producto');
     }
 }
